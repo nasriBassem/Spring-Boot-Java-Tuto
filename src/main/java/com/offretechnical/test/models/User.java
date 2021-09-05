8e6d92d33d1a12756b4db4b1bd346563b5fc46d2
bNasri <b.nasri@altereo.fr>
2021-09-05 20:26:21 +0200

    Remove Alerte Message

--- src/main/java/com/offretechnical/test/models/User.java
+++ src/main/java/com/offretechnical/test/models/User.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.Constraint;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
